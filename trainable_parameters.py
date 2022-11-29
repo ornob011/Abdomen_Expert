c75314684af825e095250c4510f349596e688785
ornob011 <ornob011@gmail.com>
2022-11-29 22:26:48 +0600

    Find trainable parameters

---
+++ trainable_parameters.py
@@ -0,0 +1,21 @@
+from prettytable import PrettyTable
+from get_model_optimizer import get_model
+
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params += params
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+
+
+if __name__ == '__main__':
+    model = get_model()
+    count_parameters(model)
